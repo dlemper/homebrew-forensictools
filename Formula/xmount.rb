bbaaafe38571215b86584ed69819931f411a21f2
Daniel Lemper <ich@dlemper.de>
2024-03-25 07:23:03 +0100

    xmount: remove libfuse and add osxfuse as dependency

--- Formula/xmount.rb
+++ Formula/xmount.rb
@@ -19,8 +19,8 @@ class Xmount < Formula
   depends_on "pkg-config" => :build
   depends_on "afflib"
   depends_on "libewf"
-  depends_on "libfuse@2"
   depends_on "openssl@3"
+  depends_on :osxfuse
 
   def install
     ENV.prepend_path "PKG_CONFIG_PATH", Formula["openssl@3"].opt_lib/"pkgconfig"
