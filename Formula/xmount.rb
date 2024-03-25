795ab0e40d84acf06e74d8e22e1eb5ffadf96233
Daniel Lemper <ich@dlemper.de>
2024-03-25 20:38:49 +0100

    xmount: try using current deps and build suggestions

--- Formula/xmount.rb
+++ Formula/xmount.rb
@@ -2,21 +2,26 @@ class Xmount < Formula
   desc "Convert between multiple input & output disk image types"
   homepage "https://www.sits.lu/xmount"
   url "https://code.sits.lu/foss/xmount/-/archive/1.1.1/xmount-1.1.1.tar.gz"
-  sha256 "76e544cd55edc2dae32c42a38a04e11336f4985e1d59cec9dd41e9f9af9b0008"
+  sha256 "501e5f0551f03ce30b834da62a30c4bd130d2c8889444a5e592f91b098aaab45"
   license "GPL-3.0-or-later"
   revision 4
 
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
-  depends_on "afflib"
-  depends_on "libewf"
   depends_on "openssl@3"
-  depends_on cask: "macfuse"
+  depends_on "cppunit"
+  depends_on "autoconf"
+  depends_on "automake"
+  depends_on "cmake"
+  depends_on "libtool"
+  depends_on "readline"
+  #depends_on cask: "macfuse"
 
   def install
     ENV.prepend_path "PKG_CONFIG_PATH", Formula["openssl@3"].opt_lib/"pkgconfig"
 
-    system "cmake", ".", *std_cmake_args
+    system "./BuildLibs.sh"
+    system "cmake", "-DCMAKE_BUILD_TYPE=Releas", "-DSTATIC=1", ".", *std_cmake_args
     system "make", "install"
   end
 
