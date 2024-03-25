ebdf9d8167d834f1cddb203d444f7331845bb9fe
Daniel Lemper <ich@dlemper.de>
2024-03-25 07:33:38 +0100

    xmount: use new version and link to new website

--- Formula/xmount.rb
+++ Formula/xmount.rb
@@ -1,25 +1,17 @@
 class Xmount < Formula
   desc "Convert between multiple input & output disk image types"
-  homepage "https://www.pinguin.lu/xmount/"
-  url "https://files.pinguin.lu/xmount-0.7.6.tar.gz"
+  homepage "https://www.sits.lu/xmount"
+  url "https://code.sits.lu/foss/xmount/-/archive/1.1.1/xmount-1.1.1.tar.gz"
   sha256 "76e544cd55edc2dae32c42a38a04e11336f4985e1d59cec9dd41e9f9af9b0008"
   license "GPL-3.0-or-later"
-  revision 3
-
-  livecheck do
-    url "https://deb.debian.org/debian/pool/main/x/xmount/"
-    regex(/href=.*?xmount[._-]v?(\d+(?:\.\d+)+)\.orig\.t/i)
-  end
-
-  bottle do
-    sha256 x86_64_linux: "c61ddafea43cbd071198031f80f4893a2fc90266a9daf1999e78a1b6211d748a"
-  end
+  revision 4
 
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
   depends_on "afflib"
   depends_on "libewf"
   depends_on "openssl@3"
+  depends_on cask: "macfuse"
 
   def install
     ENV.prepend_path "PKG_CONFIG_PATH", Formula["openssl@3"].opt_lib/"pkgconfig"
