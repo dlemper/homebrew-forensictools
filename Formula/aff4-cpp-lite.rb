70247fab7b6f012c0e573b5bf1d82bc3c6085e05
Daniel Lemper <ich@dlemper.de>
2024-03-28 10:08:50 +0100

    aff4-cpp-lite: fix building

--- Formula/aff4-cpp-lite.rb
+++ Formula/aff4-cpp-lite.rb
@@ -8,14 +8,22 @@ class Aff4CppLite < Formula
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
+  depends_on "cppunit" => :build
+  depends_on "openssl@3"
   depends_on "snappy"
   depends_on "raptor"
   depends_on "libxml2"
+  depends_on "lz4"
   depends_on "pkg-config"
 
   def install
+    #ENV.append_to_cflags
+    ENV["CFLAGS"] = "-fPIC -DO_LARGEFILE=0x0 -Dpread64=pread"
+    ENV["CXXFLAGS"] = "-fPIC -DO_LARGEFILE=0x0 -Dpread64=pread -std=c++11"
+
+    system "sed", "-i.bak", "s#/opt/local#/opt/homebrew#", "autogen.sh"
     system "./autogen.sh"
-    system "./configure", *std_configure_args
+    system "./configure", "--prefix="+self.prefix
     system "make", "install"
   end
 end
