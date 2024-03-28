9583bc689331eb02cc07ac938c354a2ca411ae79
Daniel Lemper <ich@dlemper.de>
2024-03-28 10:51:49 +0100

    xmount: fix missing fuse deps

--- Formula/xmount.rb
+++ Formula/xmount.rb
@@ -1,3 +1,30 @@
+# copied from https://github.com/borgbackup/homebrew-tap/blob/master/Formula/borgbackup-fuse.rb
+class OsxfuseRequirement < Requirement
+  fatal true
+
+  satisfy(build_env: false) { self.class.binary_osxfuse_installed? }
+
+  def self.binary_osxfuse_installed?
+    File.exist?("/usr/local/include/fuse/fuse.h") &&
+      !File.symlink?("/usr/local/include/fuse")
+  end
+
+  env do
+    ENV.append_path "PKG_CONFIG_PATH",
+                    "/usr/local/lib/pkgconfig:#{HOMEBREW_PREFIX}/lib/pkgconfig:" \
+                    "#{HOMEBREW_PREFIX}/opt/openssl@1.1/lib/pkgconfig"
+
+    if HOMEBREW_PREFIX.to_s != "/usr/local"
+      ENV.append_path "HOMEBREW_LIBRARY_PATHS", "/usr/local/lib"
+      ENV.append_path "HOMEBREW_INCLUDE_PATHS", "/usr/local/include/fuse"
+    end
+  end
+
+  def message
+    "macFUSE is required to build borgbackup-fuse. Please run `brew install --cask macfuse` first."
+  end
+end
+
 class Xmount < Formula
   desc "Convert between multiple input & output disk image types"
   homepage "https://www.sits.lu/xmount"
@@ -6,6 +33,7 @@ class Xmount < Formula
   license "GPL-3.0-or-later"
   revision 4
 
+  depends_on OsxfuseRequirement => :build
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
   depends_on "autoconf" => :build
@@ -19,7 +47,6 @@ class Xmount < Formula
   depends_on "afflib"
   depends_on "libewf"
   depends_on "aff4-cpp-lite"
-  #depends_on cask: "macfuse"
 
   def install
     ENV.prepend_path "PKG_CONFIG_PATH", Formula["openssl@3"].opt_lib/"pkgconfig"
@@ -29,7 +56,6 @@ class Xmount < Formula
     ENV.prepend_path "PKG_CONFIG_PATH", Formula["afflib"].opt_lib/"pkgconfig"
     ENV.prepend_path "PKG_CONFIG_PATH", Formula["libxml2"].opt_lib/"pkgconfig"
     ENV.prepend_path "PKG_CONFIG_PATH", Formula["libewf"].opt_lib/"pkgconfig"
-    ENV.append_path "PKG_CONFIG_PATH", "/usr/local/lib/pkgconfig"
 
     system "echo", "cmake", *std_cmake_args
     system "make", "install"
